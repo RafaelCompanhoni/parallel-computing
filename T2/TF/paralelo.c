d319327d43fe62fe1aeffd96702d0aecdebacfe7
Rafael Companhoni <companhoni@gmail.com>
2019-06-12 16:28:09 -0300

    Populate matrix function

--- T2/TF/paralelo.c
+++ T2/TF/paralelo.c
@@ -6,6 +6,20 @@
 int m1[SIZE][SIZE], m2[SIZE][SIZE], mres[SIZE][SIZE];
 int l1, c1, l2, c2, lres, cres;
 
+void printMatrix(matrix)
+{
+    int i, j;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            printf("%d     ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int initializeMatrixes()
 {
     int i, j;
@@ -24,7 +38,7 @@ int initializeMatrixes()
         k++;
     }
 
-    // initialize matrix m2  
+    // initialize matrix m2
     k = 1;
     for (j = 0; j < SIZE; j++)
     {
@@ -37,31 +51,38 @@ int initializeMatrixes()
         }
         k++;
     }
+}
 
-    // print both
-    printf("MATRIZ 1:\n");
+int multiply()
+{
+    int i, j;
+
     for (i = 0; i < SIZE; i++)
     {
         for (j = 0; j < SIZE; j++)
         {
-            printf("%d     ", m1[i][j]);
+            mres[i][j] = 0;
+            for (k = 0; k < c1; k++)
+            {
+                mres[i][j] += m1[i][k] * m2[k][j];
+            }
         }
-        printf("\n");
-    }
-
-    printf("\nMATRIZ 2:\n");
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            printf("%d     ", m2[i][j]);
-        }
-        printf("\n");
     }
 }
 
 int main(int argc, char *argv[])
 {
     initializeMatrixes();
+
+    printf("MATRIZ 1:\n");
+    printMatrix(m1);
+
+    printf("MATRIZ 2:\n");
+    printMatrix(m2);
+
+    multiply();
+    printf("\nRESULT:\n");
+    printMatrix(mres);
+
     return 0;
 }
