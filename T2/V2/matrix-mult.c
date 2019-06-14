42cdd104b301c97b8b134488962c51ae968496cb
Rafael Companhoni <companhoni@gmail.com>
2019-06-14 18:06:43 -0300

    V2

--- T2/V2/matrix-mult.c
+++ T2/V2/matrix-mult.c
@@ -104,7 +104,7 @@ main(int argc, char **argv)
 
             // update index for next iteration
             currentRowToProcess += worker_requester_capacity;
-            printf("[MESTRE] - processado %d de %d", currentRowToProcess, SIZE);
+            printf("[MESTRE] - processado %d de %d\n", currentRowToProcess, SIZE);
         } while(currentRowToProcess < SIZE);
     }
     else
