b69e3135b4a4bb966ea42f597dc796870ab93bbb
Rafael Companhoni <companhoni@gmail.com>
2019-06-15 17:16:06 -0300

    V2

--- T2/V2/matrix-mult.c
+++ T2/V2/matrix-mult.c
@@ -115,7 +115,7 @@ main(int argc, char **argv)
 
             // send stop condition to worker
             int stopWorker = 0;
-            if (currentRowToProcess < SIZE) {
+            if (currentRowToProcess == SIZE) {
                 printf("[MESTRE] - enviando mensagem de encerramento ao escravo[%d]\n", status.MPI_SOURCE);
                 stopWorker = 1;
             }
