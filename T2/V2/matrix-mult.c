e043224c68632d89f720cdb28a5da409af897512
Rafael Companhoni <companhoni@gmail.com>
2019-06-15 23:40:11 -0300

    V2

--- T2/V2/matrix-mult.c
+++ T2/V2/matrix-mult.c
@@ -163,7 +163,7 @@ main(int argc, char **argv)
             // receives batch from the master
             MPI_Recv(&batch_to_process, workerCapacity*SIZE, MPI_INT, 0, RESPONSE_BATCH_TAG, MPI_COMM_WORLD, &status);
             printf("[ESCRAVO-%d] - recebido batch para processar\n", my_rank);
-            printMatrix(workerCapacity, SIZE, &batch_to_process);
+            printMatrix(workerCapacity, SIZE, batch_to_process);
 
             // multiply partialMatrix with base matrix 'm2'
             int i, j, k;
@@ -182,7 +182,7 @@ main(int argc, char **argv)
 
             // sends results back to the master
             printf("[ESCRAVO-%d] - enviando resultados parciais\n", my_rank);
-            printMatrix(workerCapacity, SIZE, &partialResult);
+            printMatrix(workerCapacity, SIZE, partialResult);
             MPI_Send(&partialResult, workerCapacity*SIZE, MPI_INT, 0, PARTIAL_RESULT_TAG, MPI_COMM_WORLD); 
 
             // check if the worker should stop
