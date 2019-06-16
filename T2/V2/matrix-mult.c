51ebd43684fd802ecb4cfe60c39b5bce750ed309
Rafael Companhoni <companhoni@gmail.com>
2019-06-15 23:27:01 -0300

    V2

--- T2/V2/matrix-mult.c
+++ T2/V2/matrix-mult.c
@@ -172,7 +172,7 @@ main(int argc, char **argv)
             {
                 for (j = 0; j < SIZE; j++)
                 {
-                    partialResult[i][j] = 0;
+                    partialResult[i * SIZE + j] = 0;
                     for (k = 0; k < SIZE; k++)
                     {
                         partialResult[i * SIZE + j] += batch_to_process[i * SIZE + k] * base_matrix[k][j];
